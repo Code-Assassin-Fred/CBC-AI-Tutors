baac83f286729a8bd76d9b1e3bf2ffa43bfa4e92
Fredrick <freddyjames534@gmail.com>
2026-01-09 15:10:55 +0300

    Build debugging

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import HeroSection from "@/components/landing/HeroSection";
-import Footer from "@/components/landing/Footer";
+import HeroSection from "@/components/Landing/HeroSection";
+import Footer from "@/components/Landing/Footer";
 
 export default function Home() {
   return (
