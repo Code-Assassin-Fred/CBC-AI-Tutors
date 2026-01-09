4e0294dd878d399ef0562cf3b25bda0c704ae24c
Fredrick <freddyjames534@gmail.com>
2026-01-09 14:36:34 +0300

    Remove unncessary fields components causing build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import HeroSection from "@/components/Landing/HeroSection";
-import Fields from "@/components/Landing/Fields";
 import Footer from "@/components/Landing/Footer";
 
 export default function Home() {
   return (
     <main className="overflow-x-hidden">
       <HeroSection />
-      <Fields />
       <Footer />
     </main>
   );
